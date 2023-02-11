98b58e93041e8f1be2758a495df99ad15efcf782
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-02-11 17:17:59 -0600

    created cards on Cta page

--- client/src/pages/Cta.js
+++ client/src/pages/Cta.js
@@ -4,7 +4,28 @@ const Cta = () => {
     return (
         <section className="">
             <div className="">
-                <h1 className=""></h1>
+
+                <div id="card1" className="">
+                <h1 className="">Keep track of your spending</h1>
+                <img id='chartImage' src=''></img>
+                </div>
+
+                <div id="card2" className="">
+                <h1 className="">Have a receipt?</h1>
+                <h1 className="">Take a photo and import it!</h1>
+                <img id='cameraicon' src=''></img>
+                </div>
+                
+                <div id="card3" className="">
+                <h1 className="">Keep track of your spending</h1>
+                <img id='chartImage' src=''></img>
+                </div>
+
+                <div id="card4" className="">
+                <h1 className="">We also have a potato as mascot</h1>
+                <h2 className="">His name is Spuddy</h2>
+                <img id='Spuddy' src=''></img>
+                </div>
             </div>
         </section>
     )
