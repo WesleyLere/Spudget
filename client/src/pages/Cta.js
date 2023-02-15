193253c1e5da3e13bde7fd4e055a0eff86725681
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-02-14 18:36:07 -0600

    cta closer to be mobile friendly

--- client/src/pages/Cta.js
+++ client/src/pages/Cta.js
@@ -2,71 +2,109 @@ import React from 'react';
 
 const Cta = () => {
     return (
-        <section className="">
+        <section className='sm:w-auto xl:w-auto'>
 
-            {/* cards at the top of cta page to showcase bige features of spudget'*/}
-            <div className="">
-                <div id="card1" className="">
-                <h1 className="">Keep track of your spending</h1>
-                <img id='chartImage' src=''></img>
-                </div>
+            <div className='xl:p-20 sm:p-5 xl:bg-gradient-to-t from-lime-600 to-lime-500 xl:flex xl:flex-row xl:items-center xl:justify-center'>
 
-                <div id="card2" className="">
-                <h1 className="">Have a receipt?</h1>
-                <h1 className="">Take a photo and import it!</h1>
-                <img id='cameraicon' src=''></img>
-                </div>
-                
-                <div id="card3" className="">
-                <h1 className="">Keep track of your spending</h1>
-                <img id='chartImage' src=''></img>
+                <div class="xl:bg-slate-300 sm:bg-slate-600 rounded-3xl xl:border-4 xl:border-lime-500 xl:m-5 xl:p-5 sm:m-1 sm:p-1 hover:scale-[1.04] transition ease-out duration-300 sm:flex sm:flex-col sm:items-center xl:mx-auto xl:max-w-6xl">
+                    <h2 class="xl:text-2xl sm:text-2xl sm: xl:text-center sm:text-center">Keep Track of your Spending</h2>
+                    <div class="xl:m-5 xl:p-5 sm:m-10 sm:p-10">
+                        <img class="w-full h-auto" src="samplegraph.png" alt="sample graph"></img>
+                    </div>
                 </div>
 
-                <div id="card4" className="">
-                <h1 className=''>We also have a potato as mascot</h1>
-                <h2 className=''>His name is Spuddy</h2>
-                <img className='' id='Spuddy' src=''></img>
+                <div class="xl:bg-slate-300 sm:bg-slate-500 rounded-3xl xl:border-4 xl:border-lime-500 xl:m-5 xl:p-5 sm:m-1 sm:p-1 hover:scale-[1.04] transition ease-out duration-300 sm:flex sm:flex-col sm:items-center xl:mx-auto xl:max-w-6xl">
+                    <h2 class="xl:text-2xl sm:text-2xl sm: xl:text-center sm:text-center">Keep Track of your Spending</h2>
+                    <div class="xl:m-5 xl:p-5 sm:m-10 sm:p-10">
+                        <img class="w-full h-auto" src="samplegraph.png" alt="sample graph"></img>
+                    </div>
                 </div>
-            </div>
 
-{/* This is the 'list of companies that love using spudget'*/}
-            <div className=''>
-                <h2 className=''>Google</h2>
-                <h2 className=''>NY Times</h2>
-                <h2 className=''>Wells Fargo</h2>
-                <h2 className=''>Target</h2>
-                <h2 className=''>Microsoft</h2>
-                <h2 className=''>Sony</h2>
-            </div>
+                <div class="xl:bg-slate-300 sm:bg-slate-400 rounded-3xl xl:border-4 xl:border-lime-500 xl:m-5 xl:p-5 sm:m-1 sm:p-1 hover:scale-[1.04] transition ease-out duration-300 sm:flex sm:flex-col sm:items-center xl:mx-auto xl:max-w-6xl">
+                    <h2 class="xl:text-2xl sm:text-2xl sm: xl:text-center sm:text-center">Keep Track of your Spending</h2>
+                    <div class="xl:m-5 xl:p-5 sm:m-10 sm:p-10">
+                        <img class="w-full h-auto" src="samplegraph.png" alt="sample graph"></img>
+                    </div>
+                </div>
 
-{/* happy family photo and other things to note about Spudget*/}
-            <div className=''>
-                <div>
-                    <img src=''></img>
+                <div class="bg-slate-300 rounded-3xl xl:border-4 xl:border-lime-500 xl:m-5 xl:p-5 sm:m-1 sm:p-1 hover:scale-[1.04] transition ease-out duration-300 sm:flex sm:flex-col sm:items-center xl:mx-auto xl:max-w-6xl">
+                    <h2 class="xl:text-2xl sm:text-2xl sm: xl:text-center sm:text-center">Keep Track of your Spending</h2>
+                    <div class="xl:m-5 xl:p-5 sm:m-10 sm:p-10">
+                        <img class="w-full h-auto" src="samplegraph.png" alt="sample graph"></img>
+                    </div>
                 </div>
-                <div>
-                    <ul className=''>
-                        <li className=''>All of your spending in one place!</li>
-                        <li className=''>Companies all around the world continue to use Spudget</li>
-                        <li className=''>Get notified before monthly reacurring trans actions</li>
-                    </ul>
-                </div>
+
             </div>
 
 
 
-    {/* social media connections other company info*/}
-    <div className=''>
-        <div>
-            <h1>Stay Connected with Spuddy!</h1>
-            <ul>
-                <a><li className=''>Instagram</li></a>
-                <a><li className=''>Twitter</li></a>
-                <a><li className=''>Facebook</li></a>
-                <a><li className=''>TikTok</li></a>
-            </ul>
-        </div>
-    </div>
+            <React.Fragment>
+                <div className='xl:bg-gradient-to-r from-slate-400 via-slate-300 to-slate-400'>
+                    <div className="">
+                        <div className='xl:flex xl:justify-between xl:flex-row sm:flex sm:flex-wrap'>
+                            <h2 className="xl:m-5 xl:p-5 sm:m-1 sm:p-1 text-2xl sm:text-lime-500 xl:text-lime-50 xl:flex sm:flex sm:justify-center">Google</h2>
+                            <h2 className="xl:m-5 xl:p-5 sm:m-1 sm:p-1 text-2xl sm:text-lime-500 xl:text-lime-50 xl:flex sm:flex sm:justify-center">NY Times</h2>
+                            <h2 className="xl:m-5 xl:p-5 sm:m-1 sm:p-1 text-2xl sm:text-lime-500 xl:text-lime-50 xl:flex sm:flex sm:justify-center">WellsFargo</h2>
+                            <h2 className="xl:m-5 xl:p-5 sm:m-1 sm:p-1 text-2xl sm:text-lime-500 xl:text-lime-50 xl:flex sm:flex sm:justify-center">Target</h2>
+                            <h2 className="xl:m-5 xl:p-5 sm:m-1 sm:p-1 text-2xl sm:text-lime-500 xl:text-lime-50 xl:flex sm:flex sm:justify-center">Sony</h2>
+                            <h2 className="xl:m-5 xl:p-5 sm:m-1 sm:p-1 text-2xl sm:text-lime-500 xl:text-lime-50 xl:flex sm:flex sm:justify-center">Microsoft</h2>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+
+
+            <React.Fragment>
+                <div className='xl:bg-gradient-to-r from-stone-400 via-stone-300 to-stone-400 xl:w-auto'>
+                    <div className='xl:flex sm:flex sm:flex-col xl:flex-row xl:items-center xl:justify-center'>
+                        <div className="flex flex-row">
+                            <img className='xl:m-5 xl:p-5 xl:shadow-xl sm:rounded-full' src='Happyspouses.jpg'></img>
+                        </div>
+                        <div className="flex flex-col">
+                            <h2 className="m-5 p-5 text-2xl">All of your Spending in one place!</h2>
+                            <h2 className="m-5 p-5 text-2xl">Get notified before reacurring transactions!</h2>
+                            <h2 className="m-5 p-5 text-2xl">Grow your Spuds with us!</h2>
+                            <h2 className="m-5 p-5 text-2xl"></h2>
+                        </div>
+                    </div>
+
+                </div>
+
+            </React.Fragment>
+
+            <React.Fragment>
+                <section className='text-2xl bg-gradient-to-b from-lime-600 to-lime-500'>
+                    <div className='xl:flex xl:flex-row xl:justify-evenly'>
+                        <div className='flex justify-center'>
+                            <img className='p-5 m-5' src='logo.png'></img>
+                        </div>
+                        <div className='m-10 p-10 flex flex-col items-center justify-center'>
+                            <h2 className='flex-1 text-white'>Contact Us</h2>
+                            <h3 className='flex-1'>625 Potato Ave. Spuddy, ID 11123</h3>
+                        </div>
+                        <div className='m-10 p-10 flex flex-col items-center justify-center'>
+                            <h2 className='flex-1 text-white'>Get the App</h2>
+                            <h3 className='flex-1 hover:text-lime-100 cursor-pointer'>App Store</h3>
+                            <h2 className='flex-1 hover:text-lime-100 cursor-pointer'>Play Store</h2>
+                        </div>
+
+                        <div className='m-10 p-10 flex flex-col items-center justify-center'>
+                            <h2 className='flex-1 text-white'>Stay Connected</h2>
+                            <ul className='p-'>
+                                <li className='hover:text-lime-100 cursor-pointer'>Instagram</li>
+                                <li className='hover:text-lime-100 cursor-pointer'>TikTok</li>
+                                <li className='hover:text-lime-100 cursor-pointer'>Twitter</li>
+                                <li className='hover:text-lime-100 cursor-pointer'>Facebook</li>
+                            </ul>
+                            <p>
+                                Copyright © 2023 Spudget, LLC
+                                All rights reserved.
+                            </p>
+                        </div>
+
+                    </div>
+                </section>
+            </React.Fragment>
         </section>
     )
 }
--- client/src/pages/MoneyDash.js
+++ client/src/pages/MoneyDash.js
@@ -1,15 +1,61 @@
 import React from 'react';
 
-const MoneyDash =() => {
+const MoneyDash = () => {
     return (
-        <section className="">
-            <div>
-                MoneyDash
+        <section className="p-5 bg-gradient-to-t from-lime-600 to-lime-500">
+            <div className="rounded-2xl border-4">
+            {/* heading*/}
+            <section className="flex flex-row items-center justify-center">
+                <h1 className="m-5 p-5 text-3xl">Money Dashboard</h1>
+            </section >
+
+
+            {/* entire container for dashbaord*/}
+            <div className="">
+
+                {/* large graph bar line combo with month selector*/}
+                <div className="rounded-2xl shadow-xl bg-green-300">
+                    <div className="">
+                        <h2 className="">
+                            This Months Spending: 
+                            <select className="" id="month" name="month">
+                                <option>month</option>
+                                <option value="january">January</option>
+                                <option value="february">February</option>
+                                <option value="march">March</option>
+                                <option value="april">April</option>
+                                <option value="may">May</option>
+                                <option value="june">June</option>
+                                <option value="july">July</option>
+                                <option value="august">August</option>
+                                <option value="september">September</option>
+                                <option value="october">October</option>
+                                <option value="november">November</option>
+                                <option value="december">December</option>
+                            </select>
+                        </h2>
+                    </div>
+
+                </div>
+
+                {/* recent spending habits and graph selector*/}
+                <div className="flex flex-row rounded-2xl shadow-xl bg-green-300">
+                <h1>Recent Spending Habits</h1>
+                </div>
+
+
+                {/* spending money visual*/}
+                <div className="">
+
+                </div>
+
+
+            </div>
             </div>
         </section>
     )
 }
 
- 
+
 
 export default MoneyDash;
