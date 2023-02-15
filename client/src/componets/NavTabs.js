f71ec9962fa4c5ba7fbf6d8d676075cc33015eaf
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-02-14 20:22:20 -0600

    created graphs for dashboard page

--- client/src/componets/NavTabs.js
+++ client/src/componets/NavTabs.js
@@ -3,65 +3,65 @@ import React from 'react';
 // prior to logging in nav bar would show log in and sign up buttons
 //after logging in nav bar would show "dashboard" "import" and "sign out" 
 // not sure how implement that in react. 
-function NavTabs({currentPage, handlePageChange}) {
-    return (
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
 
-        <div className="bg-lime-600 flex flex-row justify-center hover:shadow-inner rounded-3xl m-10 text-xl font-semibold">
-           
-        <div className='flex flex-row justify-start'>
-            <img className='m-5 h-16' src='logo.png'></img>
-        </div>
-            <nav className="text-neutral-200 justify-center flex flex-row nav nav-tabs">
-        
-              <li className="hover:scale-[1.09] hover:text-amber-400 hover:drop-shadow-lg m-5 p-5 nav-item">
-                
-              <a href='#Login'
-                onClick={() => handlePageChange('Login')}
-                className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}
-                >
-                    <button className="">
-                        Login
-                    </button>
-                </a>
-              </li>
-              <li className="hover:scale-[1.2] hover:text-blue-400 hover:drop-shadow-lg m-5 p-5 nav-item">
-              <a href='#signup'
-                onClick={() => handlePageChange('Signup')}
-                className={currentPage === 'signup' ? 'nav-link active' : 'nav-link'}
-                >
-                    <button className=''>
-                        Sign Up
-                    </button>
+    <div className="bg-lime-600 flex flex-row justify-center hover:shadow-inner rounded-3xl m-10 text-xl font-semibold">
 
-                </a>
-              </li>
-              <li className="hover:scale-[1.2] hover:text-emerald-400 hover:drop-shadow-lg text m-5 p-5 nav-item">
-              <a href='#cta'
-                onClick={() => handlePageChange('Cta')}
-                className={currentPage === 'cta' ? 'nav-link active' : 'nav-link'}
-                >
-                    <button className=''>
-                        Cta
-                    </button>
+      <div className='flex flex-row justify-start'>
+        <img className='m-5 h-16' src='logo.png'></img>
+      </div>
+      <nav className="text-neutral-200 justify-center flex flex-row nav nav-tabs">
 
-                </a>
-              </li>
-             
-              <li className="hover:scale-[1.2] hover:text-fuchsia-400 hover:drop-shadow-lg m-5 p-5 nav-item">
-              <a href='#moneydash'
-                onClick={() => handlePageChange('Moneydash')}
-                className={currentPage === 'moneydash' ? 'nav-link active' : 'nav-link'}
-                >
-                    <button className=''>
-                        Money Dashboard
-                    </button>
+        <li className="hover:scale-[1.09] hover:text-amber-400 hover:drop-shadow-lg m-5 p-5 nav-item">
 
-                </a>
-              </li>
-            </nav>
-  
-          </div>
-    )
+          <a href='#Login'
+            onClick={() => handlePageChange('Login')}
+            className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}
+          >
+            <button className="">
+              Login
+            </button>
+          </a>
+        </li>
+        <li className="hover:scale-[1.2] hover:text-blue-400 hover:drop-shadow-lg m-5 p-5 nav-item">
+          <a href='#signup'
+            onClick={() => handlePageChange('Signup')}
+            className={currentPage === 'signup' ? 'nav-link active' : 'nav-link'}
+          >
+            <button className=''>
+              Sign Up
+            </button>
+
+          </a>
+        </li>
+        <li className="hover:scale-[1.2] hover:text-emerald-400 hover:drop-shadow-lg text m-5 p-5 nav-item">
+          <a href='#cta'
+            onClick={() => handlePageChange('Cta')}
+            className={currentPage === 'cta' ? 'nav-link active' : 'nav-link'}
+          >
+            <button className=''>
+              Cta
+            </button>
+
+          </a>
+        </li>
+
+        <li className="hover:scale-[1.2] hover:text-fuchsia-400 hover:drop-shadow-lg m-5 p-5 nav-item">
+          <a href='#moneydash'
+            onClick={() => handlePageChange('Moneydash')}
+            className={currentPage === 'moneydash' ? 'nav-link active' : 'nav-link'}
+          >
+            <button className=''>
+              Money Dashboard
+            </button>
+
+          </a>
+        </li>
+      </nav>
+
+    </div>
+  )
 }
 
 export default NavTabs;
--- client/src/pages/MoneyDash.js
+++ client/src/pages/MoneyDash.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import Plot from 'react-plotly.js'
+
 
 const MoneyDash = () => {
     return (
-        <section className="p-5 bg-gradient-to-t from-lime-600 to-lime-500">
-            <div className="rounded-2xl border-4">
+        <section className="p-5 bg-gradient-to-t from-lime-600 to-lime-500 ">
+            <div className=" rounded-2xl border-4">
             {/* heading*/}
             <section className="flex flex-row items-center justify-center">
                 <h1 className="m-5 p-5 text-3xl">Money Dashboard</h1>
             </section >
 
 
-            {/* entire container for dashbaord*/}
-            <div className="">
+            {/* entire container for dashboard*/}
+            <div className="xl:flex xl:flex-row sm:flex sm:flex-col">
 
                 {/* large graph bar line combo with month selector*/}
-                <div className="rounded-2xl shadow-xl bg-green-300">
+                <div className="xl:bg-slate-300 sm:bg-slate-500 rounded-3xl xl:border-4 xl:border-lime-500 xl:m-1 xl:p-5 sm:m-1 sm:p-1 xl:flex sm:flex sm:flex-col sm:items-center">
                     <div className="">
                         <h2 className="">
                             This Months Spending: 
@@ -35,18 +37,85 @@ const MoneyDash = () => {
                             </select>
                         </h2>
                     </div>
+                    <div id="graph">
+                     
+                        <Plot data={[
+                            {
+                            x:[10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000],
+                            y:['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'],
+                            type:'scatter',
+                            mode:'lines+markers',
+                            marker:{color:'green'},
+                        },
+                        {type:'bar',x:[10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000],
+                        y:['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun']}
+                    ]}
+                    layout={{autosize: true, title: 'Spuddy says you have spent this much!'}}
+                    id="test" className="graph" 
+                    />
+                    </div>
 
                 </div>
 
                 {/* recent spending habits and graph selector*/}
-                <div className="flex flex-row rounded-2xl shadow-xl bg-green-300">
+                <div className="xl:bg-slate-300 sm:bg-slate-500 rounded-3xl xl:border-4 xl:border-lime-500 xl:m-1 xl:p-5 sm:m-1 sm:p-1 xl:flex sm:flex sm:flex-col sm:items-center">
                 <h1>Recent Spending Habits</h1>
+                <Plot data={[
+                    {
+                        values:[16, 15, 12, 6, 5, 4, 42],
+                        labels: ["toll",
+                        "food",
+                        "parking",
+                        "transport",
+                        "accommodation",
+                        "gasoline",
+                        "telecom",
+                        "miscellaneous"],
+                        domain: {column: 0},
+                        name: 'expense',
+                        hoverinfo: 'label+percent+name',
+                        hole: .4,
+                        type: 'pie'
+                    },
+        
+                ]}
+                layout={{title:'Recent Spending Habits'}}
+                 
+                />
                 </div>
 
-
                 {/* spending money visual*/}
-                <div className="">
-
+                <div className="xl:bg-slate-300 sm:bg-slate-500 rounded-3xl xl:border-4 xl:border-lime-500 xl:m-1 xl:p-5 sm:m-1 sm:p-1 xl:flex sm:flex sm:flex-col sm:items-center">
+                <h1>Spending Tracker</h1>
+                <div className=''>
+                <Plot data={[
+                    {
+                        type:'indicator',
+                        value: 1000,
+                        dalta: {referace: 160},
+                        guage:{ axis: { visible: false, range: [0, 250] }},
+                        domain: { row: 0, column: 0 }
+                    },
+                    
+                ]}
+                layout={{ width: 600,
+                    height: 400,
+                    margin: {  },
+                    grid: {  },
+                    template:{
+                        data:{
+                            indicator: [
+                                {
+                                    title: {text: 'spending'},
+                                    mode: "number+delta+gauge",
+                                    delta: { reference: 90 }
+                                }
+                            ]
+                        }
+                    }
+                }}
+                />
+                </div>
                 </div>
 
 
@@ -55,6 +124,7 @@ const MoneyDash = () => {
         </section>
     )
 }
+
 
 
 
