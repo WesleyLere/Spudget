809459b3284d8637f539771aa2da4c6283acb989
Minh Truong <mptruong49@gmail.com>
2023-02-18 14:58:30 -0700

    typedefs

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,14 +1,52 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
 
-type: User {
-_id: ID
-email: String!
-Transactions: String!
-}
+  type Transaction {
+    _id: ID
+    date: Int
+    month: Int
+    year: Int
+    amount: Float
+    vendor: String
+    category: Category
+  }
 
-type: enum {
+  type Limit {
+    _id: ID
+    month: Int
+    year: Int
+    amount: Int
+  }
 
-}
-`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    transactions: [Transaction]
+    limits: [Limit]
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    user: User
+    transactionByMonth(month: Int!): [Transaction]
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    addTransaction(date: Int!, month: Int!, year: Int!, amount: Float!, vendor: String, category: String!): User
+    addLimit(month: Int!, year: Int!, amount: Int!): User
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
