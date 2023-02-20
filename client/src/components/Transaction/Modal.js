74b449ed3358d29d5b9893a0eb0a89b95206a9e2
Minh Truong <mptruong49@gmail.com>
2023-02-20 16:10:45 -0700

    making modal into a seperate component

--- client/src/components/Transaction/Modal.js
+++ client/src/components/Transaction/Modal.js
@@ -1,99 +1,20 @@
-import React, { useState } from 'react';
-import { Modal } from './Modal';
+import Spuddy from '../../images/logo.png'
 
-const UploadForm = () => {
-    const [file, setFile] = useState();
-    const [category, setCategory] = useState();
-    const [date, setDate] = useState('');
-    const [supplier, setSupplier] = useState('');
-    const [totalAmount, setTotalAmount] = useState(0);
-    const [isOpen, setIsOpen] = useState(false);
+export function Modal({
+  isOpen, 
+  closeModal,
+  category, 
+  date, 
+  supplier, 
+  totalAmount, 
+  onInputchange 
+}) {
+  function importRecipt() {
+    return;
+  }
 
-    function handleFile(event) {
-        setFile(event.target.files[0])
-    }
-
-    function onInputchange(event) {
-        console.log(event.target.value)
-        if (event.target.name === "category") {
-            setCategory(event.target.value);
-        } else if (event.target.name === "date") {
-            setDate(event.target.value);
-        } else if (event.target.name === "supplier") {
-            setSupplier(event.target.value);
-        } else {
-            setTotalAmount(event.target.value)
-        }
-    }
-
-
-    const submitFile = async (event) => {
-
-        event.preventDefault();
-
-        const formData = new FormData();
-        formData.append('file', file);
-
-        fetch('/upload', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.json())
-            .then(data => {
-                setCategory(data.category);
-                setDate(data.date);
-                setSupplier(data.supplier);
-                setTotalAmount(data.totalAmount);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-        openModal()
-    }
-
-    const closeModal = () => {
-        setIsOpen(false);
-    }
-
-    const openModal = () => {
-        setIsOpen(true);
-    }
-
-    return (
-        <section className="xl:flex xl:flex-col xl:flex-wrap xl:items-center justify-center xl:m-5 xl:p-5 bg-gradient-to-t from-lime-300 to-lime-500 rounded-2xl">
-            <h2 className="flex flex-col flex-wrap items-center justify-center text-slate-100 text-5xl m-5 p-5">Upload your Recipets here!</h2>
-            <form id="upload-form">
-                <div className="rounded-3xl text-xl flex flex-col flex-wrap items-center justify-center drop-shadow-2xl bg-green-600 m-10 p-5">
-                    <input type="file" name="file" onChange={handleFile} />
-                </div>
-                <div className='flex-row flex'>
-                    <button onClick={submitFile} className='text-3xl flex flex-col flex-wrap items-center justify-center m-10 p-5 bg-amber-400 rounded-full hover:bg-amber-200 hover:drop-shadow-lg hover:scale-[1.04] transition ease-out duration-300'>
-                        <input type="button" value="Upload" />
-                    </button>
-                    <button onClick={openModal} type="button" className='text-3xl flex flex-col flex-wrap items-center justify-center m-10 p-5 bg-amber-400 rounded-full hover:bg-amber-200 hover:drop-shadow-lg hover:scale-[1.04] transition ease-out duration-300'>
-                        Manual Entry
-                    </button>
-                </div>
-                <div className='flex flex-col flex-wrap items-center justify-center'>
-                    <h3 className='text-slate-700 hover:text-slate-500 drop-shadow-lg shadow-lg rounded-xl flex flex-wrap text-2xl m-5 p-5'>Step 1: Take a photo of your reciept</h3>
-                    <h3 className='text-slate-700 hover:text-slate-500 drop-shadow-lg shadow-lg rounded-xl flex flex-wrap text-2xl m-5 p-5'>Step 2: Click on choose file</h3>
-                    <h3 className='text-slate-700 hover:text-slate-500 drop-shadow-lg shadow-lg rounded-xl flex flex-wrap text-2xl m-5 p-5'>Step 3: Click on the receipt you'd like to upload</h3>
-                    <h3 className='text-slate-700 hover:text-slate-500 drop-shadow-lg shadow-lg rounded-xl flex flex-wrap text-2xl m-5 p-5'>Step 4: Click Upload</h3>
-                    <h3 className='text-slate-700 hover:text-slate-500 drop-shadow-lg shadow-lg rounded-xl flex flex-wrap text-2xl m-5 p-5'>Step 5: Thank Spuddy.</h3>
-                </div>
-
-            </form>
-
-            <Modal 
-                isOpen = {isOpen}
-                closeModal = {closeModal}
-                category = {category}
-                date = {date}
-                supplier = {supplier}
-                totalAmount = {totalAmount}
-                onInputchange = {onInputchange}
-            />
-            {/* <div className={`relative z-10 ${isOpen ? '' : 'hidden'}`}>
+  return (
+    <div className={`relative z-10 ${isOpen ? '' : 'hidden'}`}>
                 <div className="sm:flex sm:flex-wrap sm:flex-col sm:p-1 fixed inset-0 z-10 overflow-y-auto">
                     <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 
@@ -101,7 +22,7 @@ const UploadForm = () => {
                             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                 <div className="sm:flex sm:items-start">
                                     <div className="flex flex-shrink-0 items-center justify-center rounded-full">
-                                        <img className='m-2 h-16' src={Spuddy}></img>
+                                        <img className='m-2 h-16' src={Spuddy} alt="mascot Spuddy"></img>
                                     </div>
 
 
@@ -155,10 +76,6 @@ const UploadForm = () => {
                         </div>
                     </div>
                 </div>
-            </div> */}
-
-        </section>
-    )
+            </div>
+  )
 }
-
-export default UploadForm; 
