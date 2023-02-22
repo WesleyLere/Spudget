607224dfa69e88aaa8c50f258c550a88c2d08cde
WesleyLere <111530485+WesleyLere@users.noreply.github.com>
2023-02-21 19:17:59 -0600

    auth test

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -30,7 +30,7 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/dash');
+    window.location.assign('dash');
   }
 
   logout() {
